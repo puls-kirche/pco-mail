f7b39f19cf35cc8fc6d310af43d1bcdf7ff55555
PowJ2 <35706792+jsqu4re@users.noreply.github.com>
2022-11-12 18:55:50 +0000

    format file

--- pco_mail/mail.py
+++ pco_mail/mail.py
@@ -4,19 +4,20 @@ from email.MIMEBase import MIMEBase
 from email.MIMEText import MIMEText
 from email.Utils import COMMASPACE, formatdate
 from email import Encoders
-import os,datetime
+import os
+import datetime
 
 CRLF = "\r\n"
 login = "yourloging@googlemail.com"
 password = "yourpassword"
-attendees = ["first@gmail.com",     "second@example.com","third@hotmail.com"]
+attendees = ["first@gmail.com",     "second@example.com", "third@hotmail.com"]
 organizer = "ORGANIZER;CN=organiser:mailto:first"+CRLF+" @gmail.com"
 fro = "nickname <first@gmail.com>"
 
 ddtstart = datetime.datetime.now()
-dtoff = datetime.timedelta(days = 1)
-dur = datetime.timedelta(hours = 1)
-ddtstart = ddtstart +dtoff
+dtoff = datetime.timedelta(days=1)
+dur = datetime.timedelta(hours=1)
+ddtstart = ddtstart + dtoff
 dtend = ddtstart + dur
 dtstamp = datetime.datetime.now().strftime("%Y%m%dT%H%M%SZ")
 dtstart = ddtstart.strftime("%Y%m%dT%H%M%SZ")
@@ -25,34 +26,40 @@ dtend = dtend.strftime("%Y%m%dT%H%M%SZ")
 description = "DESCRIPTION: test invitation from pyICSParser"+CRLF
 attendee = ""
 for att in attendees:
-    attendee += "ATTENDEE;CUTYPE=INDIVIDUAL;ROLE=REQ-    PARTICIPANT;PARTSTAT=ACCEPTED;RSVP=TRUE"+CRLF+" ;CN="+att+";X-NUM-GUESTS=0:"+CRLF+" mailto:"+att+CRLF
-ical = "BEGIN:VCALENDAR"+CRLF+"PRODID:pyICSParser"+CRLF+"VERSION:2.0"+CRLF+"CALSCALE:GREGORIAN"+CRLF
-ical+="METHOD:REQUEST"+CRLF+"BEGIN:VEVENT"+CRLF+"DTSTART:"+dtstart+CRLF+"DTEND:"+dtend+CRLF+"DTSTAMP:"+dtstamp+CRLF+organizer+CRLF
-ical+= "UID:FIXMEUID"+dtstamp+CRLF
-ical+= attendee+"CREATED:"+dtstamp+CRLF+description+"LAST-MODIFIED:"+dtstamp+CRLF+"LOCATION:"+CRLF+"SEQUENCE:0"+CRLF+"STATUS:CONFIRMED"+CRLF
-ical+= "SUMMARY:test "+ddtstart.strftime("%Y%m%d @ %H:%M")+CRLF+"TRANSP:OPAQUE"+CRLF+"END:VEVENT"+CRLF+"END:VCALENDAR"+CRLF
+    attendee += "ATTENDEE;CUTYPE=INDIVIDUAL;ROLE=REQ-    PARTICIPANT;PARTSTAT=ACCEPTED;RSVP=TRUE" + \
+        CRLF+" ;CN="+att+";X-NUM-GUESTS=0:"+CRLF+" mailto:"+att+CRLF
+ical = "BEGIN:VCALENDAR"+CRLF+"PRODID:pyICSParser" + \
+    CRLF+"VERSION:2.0"+CRLF+"CALSCALE:GREGORIAN"+CRLF
+ical += "METHOD:REQUEST"+CRLF+"BEGIN:VEVENT"+CRLF+"DTSTART:"+dtstart + \
+    CRLF+"DTEND:"+dtend+CRLF+"DTSTAMP:"+dtstamp+CRLF+organizer+CRLF
+ical += "UID:FIXMEUID"+dtstamp+CRLF
+ical += attendee+"CREATED:"+dtstamp+CRLF+description+"LAST-MODIFIED:" + \
+    dtstamp+CRLF+"LOCATION:"+CRLF+"SEQUENCE:0"+CRLF+"STATUS:CONFIRMED"+CRLF
+ical += "SUMMARY:test "+ddtstart.strftime("%Y%m%d @ %H:%M") + \
+    CRLF+"TRANSP:OPAQUE"+CRLF+"END:VEVENT"+CRLF+"END:VCALENDAR"+CRLF
 
 eml_body = "Email body visible in the invite of outlook and outlook.com but not google calendar"
 eml_body_bin = "This is the email body in binary - two steps"
 msg = MIMEMultipart('mixed')
-msg['Reply-To']=fro
+msg['Reply-To'] = fro
 msg['Date'] = formatdate(localtime=True)
 msg['Subject'] = "pyICSParser invite"+dtstart
 msg['From'] = fro
 msg['To'] = ",".join(attendees)
 
-part_email = MIMEText(eml_body,"html")
-part_cal = MIMEText(ical,'calendar;method=REQUEST')
+part_email = MIMEText(eml_body, "html")
+part_cal = MIMEText(ical, 'calendar;method=REQUEST')
 
 msgAlternative = MIMEMultipart('alternative')
 msg.attach(msgAlternative)
 
-ical_atch = MIMEBase('application/ics',' ;name="%s"'%("invite.ics"))
+ical_atch = MIMEBase('application/ics', ' ;name="%s"' % ("invite.ics"))
 ical_atch.set_payload(ical)
 Encoders.encode_base64(ical_atch)
-ical_atch.add_header('Content-Disposition', 'attachment; filename="%s"'%("invite.ics"))
+ical_atch.add_header('Content-Disposition',
+                     'attachment; filename="%s"' % ("invite.ics"))
 
-eml_atch = MIMEBase('text/plain','')
+eml_atch = MIMEBase('text/plain', '')
 Encoders.encode_base64(eml_atch)
 eml_atch.add_header('Content-Transfer-Encoding', "")
 
